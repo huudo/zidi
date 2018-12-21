2af62bf76cd6de2e7103ef386ea10d6da3122b3a
huudo <huudt.3012@gmail.com>
2018-12-21 16:53:40 +0700

    fakakha

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,61 +17,50 @@ class ProductsTableSeeder extends Seeder
         $category_trangsuc = SubCategory::where('name','trangsuc')->first();
         $category_samnam = SubCategory::where('name','samnam')->first();
         $category_linhchi = SubCategory::where('name','linhchi')->first();
-        for($i = 0;$i  < 10000 ;$i++){
-            $product = new Product();
-            $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-            $product->title = "Kem Dưỡng da ban đêm ".$i;
-            $product->description = "Kem dưỡng gia tinh dầu nghệ";
-            $product->price_in = 110000;
-            $product->price = 120000;
-            $product->subcategory_id = $category_mypham->id;
-            $product->save();
-        }
-        for($i = 0;$i  < 10000 ;$i++){
-            $product = new Product();
-            $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-            $product->title = "Sâm nấm ".$i;
-            $product->description = "Sâm nấm";
-            $product->price_in = 110000;
-            $product->price = 120000;
-            $product->subcategory_id = $category_samnam->id;
-            $product->save();
-        }
-        
-        // $product = new Product();
-        // $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-        // $product->title = "Áo Nike";
-        // $product->description = "Áo Nike nhập khẩu nguyên chiếc";
-        // $product->price_in = 490000;
-        // $product->price = 500000;
-        // $product->subcategory_id = $category_quanao->id;
-        // $product->save();
 
-        // $product = new Product();
-        // $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-        // $product->title = "Vong bac 22 K";
-        // $product->description = "Vong Bac";
-        // $product->price_in = 14000000;
-        // $product->price = 15000000;
-        // $product->subcategory_id = $category_trangsuc->id;
-        // $product->save();
+        $product = new Product();
+        $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
+        $product->title = "Kem Dưỡng da ban đêm";
+        $product->description = "Kem dưỡng gia tinh dầu nghệ";
+        $product->price_in = 110000;
+        $product->price = 120000;
+        $product->subcategory_id = $category_mypham->id;
+        $product->save();
         
-        // $product = new Product();
-        // $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-        // $product->title = "Nhân sâm 6 tuổi";
-        // $product->description = "Nhân sâm 6 tuổi 1 hộp 5 kg";
-        // $product->price_in = 14000000;
-        // $product->price = 15000000;
-        // $product->subcategory_id = $category_samnam->id;
-        // $product->save();
+        $product = new Product();
+        $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
+        $product->title = "Áo Nike";
+        $product->description = "Áo Nike nhập khẩu nguyên chiếc";
+        $product->price_in = 490000;
+        $product->price = 500000;
+        $product->subcategory_id = $category_quanao->id;
+        $product->save();
 
-        // $product = new Product();
-        // $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
-        // $product->title = "Linh chi 1 năm";
-        // $product->description = "Linh chi";
-        // $product->price_in = 14000000;
-        // $product->price = 15000000;
-        // $product->subcategory_id = $category_linhchi->id;
-        // $product->save();
+        $product = new Product();
+        $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
+        $product->title = "Vong bac 22 K";
+        $product->description = "Vong Bac";
+        $product->price_in = 14000000;
+        $product->price = 15000000;
+        $product->subcategory_id = $category_trangsuc->id;
+        $product->save();
+        
+        $product = new Product();
+        $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
+        $product->title = "Nhân sâm 6 tuổi";
+        $product->description = "Nhân sâm 6 tuổi 1 hộp 5 kg";
+        $product->price_in = 14000000;
+        $product->price = 15000000;
+        $product->subcategory_id = $category_samnam->id;
+        $product->save();
+
+        $product = new Product();
+        $product->picture = "https://beautygarden.vn/Upload/Files/a0-12122018021031.jpg";
+        $product->title = "Linh chi 1 năm";
+        $product->description = "Linh chi";
+        $product->price_in = 14000000;
+        $product->price = 15000000;
+        $product->subcategory_id = $category_linhchi->id;
+        $product->save();
     }
 }
